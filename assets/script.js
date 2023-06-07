c52fb6371d22a664294dffd3c66ecf76aa2fc4b2
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-06-07 10:27:04 -0700

    Add interactivity plus some format modification

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,45 @@
-// var topics = "HTML, CSS, Git, JavaScript";
-// console.log(topics);
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopics = topics[Math.floor(Math.random() * topics.length)];
 
-// var topics = "HTML";
+const listTopics = () => {
+    for (var i = 0; i < topics.length; i++) {
+        console.log(topics[i])
+    }
+}
 
-// if (topics === 'HTML') {
-//     console.log("Let's study HTML!");
-// } else if (topics === 'CSS') {
-//     console.log("Let's study CSS!");
-// } else if (topics === 'Git') {
-//     console.log("Let's study Git!");
-// } else if (topics === 'JavaScript') {
-//     console.log("Let's study JavaScript!");
-// } else {
-//     console.log('Please try again!');
-// }
-
-var topics = ["HTML", "CSS", "Git", "JavaScript", "Random"];
-
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-    if (topics[x] === "HTML") {
+const randomTopic = () => {
+    if (randomTopics === 'HTML') {
         console.log("Let's study HTML!");
-    } else if (topics[x] === "CSS") {
+    } else if (randomTopics === 'CSS') {
         console.log("Let's study CSS!");
-    } else if (topics[x] === "Git") {
+    } else if (randomTopics === 'Git') {
         console.log("Let's study Git!");
-    } else if (topics[x] === "JavaScript") {
+    } else if (randomTopics === 'JavaScript') {
         console.log("Let's study JavaScript!");
     } else {
         console.log('Please try again!');
     }
 }
+
+const selectTopic = (topic) => {
+    if (topic === 'HTML') {
+        console.log("Let's study HTML!");
+        alert("Let's study HTML!");
+    } else if (topic === 'CSS') {
+        console.log("Let's study CSS!");
+        alert("Let's study CSS!");
+    } else if (topic === 'Git') {
+        console.log("Let's study Git!");
+        alert("Let's study Git!");
+    } else if (topic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+        alert("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
+}
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+randomTopic();
